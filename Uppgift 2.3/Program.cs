4827f23e7611c675d5d6d917bb5b1a496c518644
Hani <wintrix0101@gmail.com>
2026-09-11 13:09:45 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace övning2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket kännar den första personen i kr");
+            double första = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket kännar andra personen i kr");
+            double andra = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket kännar tredje personen i kr");
+            double tredje = double.Parse(Console.ReadLine());
+            double medelön = (tredje + andra + första) / 2;
+            Console.WriteLine("Medelönen av alla tre personer är " + medelön + " kr");
+        }
+    }
+}
